0917ba9540ec26d2a276450bcca35c6eb0a7ea92
owenmanach35 <owenmanach35@gmail.com>
2026-03-28 10:25:30 +0100

    Push

---
+++ .idx/dev.nix
@@ -0,0 +1,76 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    pkgs.python311Full
+    pkgs.poetry
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "christian-kohler.path-intellisense"
+      "formulahendry.code-runner"
+      "mechatroner.rainbow-csv"
+      "mhutchie.git-graph"
+      "ms-python.black-formatter"
+      "ms-python.debugpy"
+      "ms-python.flake8"
+      "ms-python.isort"
+      "ms-python.mypy-type-checker"
+      "ms-python.pylint"
+      "ms-python.python"
+      "ms-toolsai.jupyter"
+      "ms-toolsai.jupyter-keymap"
+      "ms-toolsai.jupyter-renderers"
+      "ms-toolsai.vscode-jupyter-cell-tags"
+      "ms-toolsai.vscode-jupyter-slideshow"
+      "njpwerner.autodocstring"
+      "wayou.vscode-todo-highlight"
+      "zaaack.markdown-editor"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+        poetry-config-in-project = "poetry config virtualenvs.in-project true";
+        poetry-config-max-workers = "poetry config installer.max-workers 8";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
